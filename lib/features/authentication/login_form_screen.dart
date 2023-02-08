18efb2f3cc7396e589779cf548b8927367ba60a9
baekjungkim <zvgandam@gmail.com>
2023-02-08 10:55:27 +0900

    #4.10 Login Form ScaffoldTap Add

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -21,57 +21,64 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     _formKey.currentState!.save();
   }
 
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Log in'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size36,
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Log in'),
         ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Gaps.v28,
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Email',
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Gaps.v28,
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  onEditingComplete: _onSubmitTap,
+                  validator: (value) {
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    if (newValue == null) return;
+                    formData['email'] = newValue;
+                  },
                 ),
-                onEditingComplete: _onSubmitTap,
-                validator: (value) {
-                  return null;
-                },
-                onSaved: (newValue) {
-                  if (newValue == null) return;
-                  formData['email'] = newValue;
-                },
-              ),
-              Gaps.v16,
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Password',
+                Gaps.v16,
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                  ),
+                  onEditingComplete: _onSubmitTap,
+                  validator: (value) {
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    if (newValue == null) return;
+                    formData['password'] = newValue;
+                  },
                 ),
-                onEditingComplete: _onSubmitTap,
-                validator: (value) {
-                  return null;
-                },
-                onSaved: (newValue) {
-                  if (newValue == null) return;
-                  formData['password'] = newValue;
-                },
-              ),
-              Gaps.v28,
-              GestureDetector(
-                onTap: _onSubmitTap,
-                child: const FormButton(
-                  disabled: false,
-                  text: 'Log in',
+                Gaps.v28,
+                GestureDetector(
+                  onTap: _onSubmitTap,
+                  child: const FormButton(
+                    disabled: false,
+                    text: 'Log in',
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
