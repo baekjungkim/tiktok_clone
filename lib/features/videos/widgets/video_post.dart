007fe3fb7b2049c2a2beb21f22e3654723b53ff3
Kim Baekjung <zvgandam@gmail.com>
2023-02-14 12:51:01 +0900

    7.4 VisibilityDetector

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPost extends StatefulWidget {
   final Function onVideoFinished;
+  final int index;
 
   const VideoPost({
     super.key,
     required this.onVideoFinished,
+    required this.index,
   });
 
   @override
@@ -18,20 +23,18 @@ class _VideoPostState extends State<VideoPost> {
       VideoPlayerController.asset('assets/videos/video.mp4');
 
   void _onVideoChange() {
-    if (_videoPlayerController.value.isInitialized) {
-      if (_videoPlayerController.value.duration ==
-          _videoPlayerController.value.position) {
-        widget.onVideoFinished();
-      }
+    if (!_videoPlayerController.value.isInitialized) return;
+    if (_videoPlayerController.value.duration ==
+        _videoPlayerController.value.position) {
+      widget.onVideoFinished();
     }
   }
 
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
     setState(() {
-      _videoPlayerController.play();
+      _videoPlayerController.addListener(_onVideoChange);
     });
-    _videoPlayerController.addListener(_onVideoChange);
   }
 
   @override
@@ -46,18 +49,52 @@ class _VideoPostState extends State<VideoPost> {
     super.dispose();
   }
 
+  void _onVisibilityChanged(VisibilityInfo info) {
+    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.play();
+    }
+  }
+
+  void _onTogglePause() {
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+    } else {
+      _videoPlayerController.play();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: _videoPlayerController.value.isInitialized
-              ? VideoPlayer(_videoPlayerController)
-              : Container(
-                  color: Colors.black,
+    return VisibilityDetector(
+      key: Key('${widget.index}'),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
+                : Container(
+                    color: Colors.black,
+                  ),
+          ),
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: _onTogglePause,
+            ),
+          ),
+          const Positioned.fill(
+            child: IgnorePointer(
+              child: Center(
+                child: FaIcon(
+                  FontAwesomeIcons.play,
+                  color: Colors.white,
+                  size: Sizes.size64,
                 ),
-        ),
-      ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
