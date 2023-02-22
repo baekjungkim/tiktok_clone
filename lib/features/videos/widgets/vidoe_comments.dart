b2eb7078778661948e3daedeeca41068418db79e
baekjungkim <zvgandam@gmail.com>
2023-02-22 10:27:03 +0900

    #8.4 Conclusions

--- lib/features/videos/widgets/vidoe_comments.dart
+++ lib/features/videos/widgets/vidoe_comments.dart
@@ -13,6 +13,8 @@ class VideoComments extends StatefulWidget {
 class _VideoCommentsState extends State<VideoComments> {
   bool _isWriting = false;
 
+  final ScrollController _scrollController = ScrollController();
+
   void _onClosePressed() {
     Navigator.of(context).pop();
   }
@@ -59,56 +61,62 @@ class _VideoCommentsState extends State<VideoComments> {
           onTap: _stopCommenting,
           child: Stack(
             children: [
-              ListView.separated(
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size10,
-                  horizontal: Sizes.size16,
-                ),
-                separatorBuilder: (context, index) => Gaps.v20,
-                itemCount: 10,
-                itemBuilder: (context, index) => Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const CircleAvatar(
-                      radius: Sizes.size20,
-                      child: Text('baek'),
-                    ),
-                    Gaps.h10,
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'baek',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: Sizes.size14,
-                                color: Colors.grey.shade500),
-                          ),
-                          Gaps.v3,
-                          const Text(
-                              "That's not it l've seen the save thing but also in a cave"),
-                        ],
+              Scrollbar(
+                controller: _scrollController,
+                child: ListView.separated(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.only(
+                    top: Sizes.size10,
+                    left: Sizes.size16,
+                    right: Sizes.size16,
+                    bottom: Sizes.size96 + Sizes.size20,
+                  ),
+                  separatorBuilder: (context, index) => Gaps.v20,
+                  itemCount: 10,
+                  itemBuilder: (context, index) => Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const CircleAvatar(
+                        radius: Sizes.size20,
+                        child: Text('baek'),
                       ),
-                    ),
-                    Gaps.h10,
-                    Column(
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size20,
-                          color: Colors.grey.shade500,
+                      Gaps.h10,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'baek',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Sizes.size14,
+                                  color: Colors.grey.shade500),
+                            ),
+                            Gaps.v3,
+                            const Text(
+                                "That's not it l've seen the save thing but also in a cave"),
+                          ],
                         ),
-                        Gaps.v5,
-                        Text(
-                          '52.5K',
-                          style: TextStyle(
+                      ),
+                      Gaps.h10,
+                      Column(
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size20,
                             color: Colors.grey.shade500,
                           ),
-                        ),
-                      ],
-                    )
-                  ],
+                          Gaps.v5,
+                          Text(
+                            '52.5K',
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
               Positioned(
@@ -175,7 +183,7 @@ class _VideoCommentsState extends State<VideoComments> {
                                         FontAwesomeIcons.faceSmile,
                                         color: Colors.black,
                                       ),
-                                      Gaps.h14,
+                                      if (_isWriting) Gaps.h14,
                                       if (_isWriting)
                                         GestureDetector(
                                           onTap: _stopCommenting,
