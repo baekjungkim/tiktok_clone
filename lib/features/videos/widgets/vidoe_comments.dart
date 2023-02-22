5e6a0965b3d876c9a93fc41be30cddb297bd6109
baekjungkim <zvgandam@gmail.com>
2023-02-22 09:26:12 +0900

    #8.1 Comments

--- lib/features/videos/widgets/vidoe_comments.dart
+++ lib/features/videos/widgets/vidoe_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -36,10 +37,71 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
           itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            child: const Text('commnet'),
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: Sizes.size20,
+                child: Text('baek'),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'baek',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500),
+                    ),
+                    Gaps.v3,
+                    const Text(
+                        "That's not it l've seen the save thing but also in a cave"),
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    size: Sizes.size20,
+                    color: Colors.grey.shade500,
+                  ),
+                  Gaps.v5,
+                  Text(
+                    '52.5K',
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: const [
+              CircleAvatar(
+                radius: Sizes.size20,
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                foregroundImage: NetworkImage(
+                    'https://imagedelivery.net/-4c3-zEb4Op0_1qjNCTcBg/83d21280-3efd-4f64-1e5b-deaf86e0d900/avatar'),
+                child: Text('vgan'),
+              ),
+            ],
           ),
         ),
       ),
