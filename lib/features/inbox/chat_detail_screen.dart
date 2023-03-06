73c47539c869f8dc1116a179d37062a19ec88538
Kim Baekjung <zvgandam@gmail.com>
2023-03-06 18:32:51 +0900

    #11.4 Chat Detail v2

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -33,6 +33,12 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
   //   }
   //   setState(() {});
   // }
+  void _stopChats() {
+    FocusScope.of(context).unfocus();
+    // setState(() {
+    //   isWriting = false;
+    // });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -96,136 +102,143 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
           ),
         ),
       ),
-      body: Stack(
-        children: [
-          ListView.separated(
-            padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size20,
-              horizontal: Sizes.size14,
-            ),
-            itemBuilder: (context, index) {
-              final isMine = index % 2 == 0;
-              return Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment:
-                    isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(
-                      Sizes.size14,
-                    ),
-                    decoration: BoxDecoration(
-                      color:
-                          isMine ? Colors.blue : Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(
-                          Sizes.size16,
-                        ),
-                        topRight: const Radius.circular(
-                          Sizes.size16,
-                        ),
-                        bottomLeft: Radius.circular(
-                          isMine ? Sizes.size16 : Sizes.size2,
-                        ),
-                        bottomRight: Radius.circular(
-                          isMine ? Sizes.size2 : Sizes.size16,
-                        ),
-                      ),
-                    ),
-                    child: const Text(
-                      "this is a messages!",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: Sizes.size16,
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            },
-            separatorBuilder: (context, index) => Gaps.v10,
-            itemCount: 10,
-          ),
-          Positioned(
-            bottom: 0,
-            width: MediaQuery.of(context).size.width,
-            child: BottomAppBar(
-              color: Colors.grey.shade100,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size10,
-                  horizontal: Sizes.size10,
-                ),
-                child: Row(
+      body: GestureDetector(
+        onTap: _stopChats,
+        child: Stack(
+          children: [
+            ListView.separated(
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size20,
+                horizontal: Sizes.size14,
+              ),
+              itemBuilder: (context, index) {
+                final isMine = index % 2 == 0;
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment:
+                      isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: SizedBox(
-                        height: Sizes.size44,
-                        child: TextField(
-                          controller: _textEditingController,
-                          cursorColor: Theme.of(context).primaryColor,
-                          decoration: InputDecoration(
-                            hintText: 'Send a message...',
-                            hintStyle: TextStyle(
-                              color: Colors.grey.shade500,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                Sizes.size24,
-                              ),
-                              borderSide: BorderSide.none,
-                            ),
-                            contentPadding: const EdgeInsets.symmetric(
-                              horizontal: Sizes.size14,
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            suffixIcon: Padding(
-                              padding: const EdgeInsets.only(
-                                right: Sizes.size10,
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  FaIcon(
-                                    FontAwesomeIcons.faceSmile,
-                                    color: Colors.grey.shade500,
-                                  )
-                                ],
-                              ),
-                            ),
+                    Container(
+                      padding: const EdgeInsets.all(
+                        Sizes.size14,
+                      ),
+                      decoration: BoxDecoration(
+                        color: isMine
+                            ? Colors.blue
+                            : Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(
+                            Sizes.size16,
                           ),
+                          topRight: const Radius.circular(
+                            Sizes.size16,
+                          ),
+                          bottomLeft: Radius.circular(
+                            isMine ? Sizes.size16 : Sizes.size2,
+                          ),
+                          bottomRight: Radius.circular(
+                            isMine ? Sizes.size2 : Sizes.size16,
+                          ),
+                        ),
+                      ),
+                      child: const Text(
+                        "this is a messages!",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: Sizes.size16,
                         ),
                       ),
                     ),
-                    if (isWriting) Gaps.h14,
-                    AnimatedOpacity(
-                      opacity: isWriting ? 1 : 0,
-                      duration: const Duration(milliseconds: 200),
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 200),
-                        height: isWriting ? Sizes.size40 : 0,
-                        width: isWriting ? Sizes.size40 : 0,
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade400,
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Center(
-                          child: FaIcon(
-                            FontAwesomeIcons.paperPlane,
-                            size: Sizes.size20,
-                            color: Colors.white,
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => Gaps.v10,
+              itemCount: 10,
+            ),
+            Positioned(
+              bottom: 0,
+              width: MediaQuery.of(context).size.width,
+              child: BottomAppBar(
+                color: Colors.grey.shade100,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: Sizes.size10,
+                    horizontal: Sizes.size10,
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: Sizes.size44,
+                          child: TextField(
+                            controller: _textEditingController,
+                            cursorColor: Theme.of(context).primaryColor,
+                            textInputAction: TextInputAction.newline,
+                            minLines: null,
+                            maxLines: null,
+                            decoration: InputDecoration(
+                              hintText: 'Send a message...',
+                              hintStyle: TextStyle(
+                                color: Colors.grey.shade500,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(
+                                  Sizes.size24,
+                                ),
+                                borderSide: BorderSide.none,
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: Sizes.size14,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              suffixIcon: Padding(
+                                padding: const EdgeInsets.only(
+                                  right: Sizes.size10,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    FaIcon(
+                                      FontAwesomeIcons.faceSmile,
+                                      color: Colors.grey.shade500,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    )
-                  ],
+                      if (isWriting) Gaps.h14,
+                      AnimatedOpacity(
+                        opacity: isWriting ? 1 : 0,
+                        duration: const Duration(milliseconds: 200),
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          height: isWriting ? Sizes.size40 : 0,
+                          width: isWriting ? Sizes.size40 : 0,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Center(
+                            child: FaIcon(
+                              FontAwesomeIcons.paperPlane,
+                              size: Sizes.size20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
