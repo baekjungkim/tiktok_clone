c420e176e849f28c585a077c3b9c506494ce93de
baekjungkim <zvgandam@gmail.com>
2023-03-13 22:53:14 +0900

    15.1 ThemeMode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,8 +32,13 @@ class _TiktokAppState extends State<TiktokApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ticktok Clone',
+      themeMode: ThemeMode.system,
       theme: ThemeData(
+        brightness: Brightness.light,
         scaffoldBackgroundColor: Colors.white,
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.grey.shade50,
+        ),
         primaryColor: const Color(0xffe9435a),
         textSelectionTheme: const TextSelectionThemeData(
           cursorColor: Color(0xffe9435a),
@@ -52,7 +57,15 @@ class _TiktokAppState extends State<TiktokApp> {
           ),
         ),
       ),
-      home: const MainNavigationScreen(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.grey.shade800,
+        ),
+        primaryColor: const Color(0xffe9435a),
+      ),
+      home: const SignUpScreen(),
     );
   }
 }
