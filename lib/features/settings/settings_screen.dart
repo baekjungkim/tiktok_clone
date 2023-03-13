8fa771dbd45271d41945a0d2ad9687e9f6a791a2
Kim Baekjung <zvgandam@gmail.com>
2023-03-13 10:52:27 +0900

    #13.1 AboutListTile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -11,9 +11,21 @@ class SettingsScreen extends StatelessWidget {
           "Settings",
         ),
       ),
-      body: Column(
-        children: const [
-          CircularProgressIndicator.adaptive(),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () => showAboutDialog(
+              context: context,
+              applicationVersion: "1.0",
+              applicationLegalese: "All rights reserved. Please dont copy me.",
+            ),
+            title: const Text(
+              "About",
+              style: TextStyle(fontWeight: FontWeight.w600),
+            ),
+            subtitle: const Text("About this app..."),
+          ),
+          const AboutListTile(),
         ],
       ),
     );
