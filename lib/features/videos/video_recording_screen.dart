b092ecffc39e56b4f9c381af7ec160011b8818d5
Kim Baekjung <zvgandam@gmail.com>
2023-03-16 15:24:10 +0900

    19.6 startVideoRecording

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/videos/video_preview_screen.dart';
 import 'package:tiktok_clone/features/videos/widgets/flash_mode_button.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
@@ -49,7 +50,11 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     }
     _cameraController = CameraController(
         cameras[_isSelfieMode ? 1 : 0], ResolutionPreset.ultraHigh);
+
     await _cameraController.initialize();
+
+    await _cameraController.prepareForVideoRecording();
+
     _flashMode = _cameraController.value.flashMode;
   }
 
@@ -117,14 +122,37 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     setState(() {});
   }
 
-  void _startRecording(TapDownDetails _) {
+  Future<void> _startRecording(TapDownDetails _) async {
+    if (_cameraController.value.isRecordingVideo) return;
+
+    await _cameraController.startVideoRecording();
+
     _buttonAnimationController.forward();
     _progressAnimationController.forward();
   }
 
-  void _stopRecording() {
+  Future<void> _stopRecording() async {
+    if (!_cameraController.value.isRecordingVideo) return;
+
     _buttonAnimationController.reverse();
     _progressAnimationController.reset();
+
+    final video = await _cameraController.stopVideoRecording();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(video: video),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _progressAnimationController.dispose();
+    _buttonAnimationController.dispose();
+    _cameraController.dispose();
+    super.dispose();
   }
 
   @override
@@ -177,8 +205,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
                     child: GestureDetector(
                       onTapDown: _startRecording,
                       onTapUp: (details) => _stopRecording(),
-                      onVerticalDragEnd: (details) => _stopRecording(),
-                      onHorizontalDragEnd: (details) => _stopRecording(),
+                      onLongPressEnd: (details) => _stopRecording(),
                       child: ScaleTransition(
                         scale: _buttonAnimation,
                         child: Stack(
